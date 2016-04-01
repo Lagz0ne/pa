e7dad4d00ff0a8dec7fbf380e7c35c0cb80c68df
Duc Dao <duc@ego-pulse.com>
2016-04-01 17:45:29 +0700

    Updated layout

--- app/containers/ResultPanel.jsx
+++ app/containers/ResultPanel.jsx
@@ -6,9 +6,23 @@ import _ from 'lodash';
 
 import Card from 'material-ui/lib/card/card';
 import CardActions from 'material-ui/lib/card/card-actions';
+import CardText from 'material-ui/lib/card/card-text';
 import Divider from 'material-ui/lib/divider';
 import CardTitle from 'material-ui/lib/card/card-title';
+
+/** Buttons **/
 import RaisedButton from 'material-ui/lib/raised-button';
+import FlatButton from 'material-ui/lib/flat-button';
+
+/** icons **/
+import DateIcon from 'material-ui/lib/svg-icons/action/date-range';
+import PhoneIcon from 'material-ui/lib/svg-icons/communication/phonelink-ring';;
+import MailIcon from 'material-ui/lib/svg-icons/communication/mail-outline';;
+
+/** Colors **/
+import Colors from 'material-ui/lib/styles/colors';
+
+import fecha from 'fecha';
 
 import * as eventActions from '../actions/eventActions';
 
@@ -34,18 +48,82 @@ class ResultPanel extends Component {
     }
   }
 
+  _convertDate = (date) => {
+    const convertedDate = fecha.parse(date, 'YYYY-M-D');
+    return fecha.format(convertedDate, 'YYYY-MM-DD');
+  }
+
   render() {
     return (
       <Row>
         {_.map(this.props.searchResult, result => (
           <Col xs={12} sm={6} key={result.id} style={paddingBottom}>
             <Card>
-              <CardTitle title={result.name} subtitle={`${result.birthDate} - ${result.email || 'No email'} - ${result.phone || 'No phone'}`}/>
-              <Divider />
+              <CardTitle title={result.name} />
+
+              <CardText>
+                <Row top="xs">
+                  <Col xs={8}>
+                    <RaisedButton
+                      disabled={true}
+                      label={this._convertDate(result.birthDate)}
+                      labelPosition="after"
+                      linkButton={true}
+                      disabledBackgroundColor={Colors.white}
+                      disabledLabelColor={Colors.grey800}
+                      icon={<DateIcon />}
+                    />
+                    <br/>
+                    <RaisedButton
+                      disabled={true}
+                      label={`${result.email || 'N/A'} `}
+                      labelStyle={{textTransform: 'lowercase'}}
+                      disabledBackgroundColor={Colors.white}
+                      disabledLabelColor={Colors.grey800}
+                      linkButton={true}
+                      icon={<MailIcon />}
+                    />
+                    <br/>
+                    <RaisedButton
+                      disabled={true}
+                      label={`${result.phone || 'N/A'} `}
+                      labelPosition="after"
+                      labelStyle={{textTransform: 'lowercase'}}
+                      disabledBackgroundColor={Colors.white}
+                      disabledLabelColor={Colors.grey800}
+                      linkButton={true}
+                      icon={<PhoneIcon />}
+                    />
+
+                  </Col>
+                  <Col xs={4}>
+                    <RaisedButton
+                      disabled={true}
+                      disabledBackgroundColor={Colors.indigo400}
+                      disabledLabelColor={Colors.white}
+                      fullWidth={true}
+                      style={{paddingBottom: '5px'}}
+                      label={result.registrationNumber}
+                      />
+
+                    <RaisedButton
+                      disabled={true}
+                      disabledBackgroundColor={Colors.indigo400}
+                      disabledLabelColor={Colors.white}
+                      fullWidth={true}
+                      label={result.tShirt}
+                      />
+                  </Col>
+                </Row>
+              </CardText>
+
               <CardActions>
                 <RaisedButton
                   disabled={result.pickedUp}
-                  label={`${result.pickedUp ? 'Picked up' : 'Pick up'} | ${result.tShirt} shirt | ${result.registrationNumber}`} primary={true}
+                  fullWidth={true}
+                  disabledBackgroundColor={Colors.grey100}
+                  disabledLabelColor={Colors.grey500}
+                  label={`${result.pickedUp ? 'Picked up' : 'Pick up'}`} primary={true}
                   onClick={this.onPickupClicked(result.id)}
                   />
               </CardActions>
